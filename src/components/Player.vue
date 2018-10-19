f850619a1d46947ae7f1e95f138f6257c1f3889b
mimizukmsk <mizuku3k@gmail.com>
2018-10-20 00:34:02 +0900

    Rebuild App as SPA with Vue.js

---
+++ src/components/Player.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="player">
+    <button>Play</button>
+    <button>Stop</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Player'
+}
+</script>
+
+<style scoped>
+
+</style>
+
